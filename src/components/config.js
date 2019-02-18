f1e6b40931faa1f1713fb499f71aff1c9916189d
Jonathan Pulido <jonh19h@gmail.com>
2019-02-18 16:52:36 -0600

    ...

--- src/components/config.js
+++ src/components/config.js
@@ -1,7 +1,7 @@
 console.log('Enviroment: '+process.env.NODE_ENV)
 export const API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://facer-recognition.herokuapp.com/'
-  : 'https://facer-recognition.herokuapp.com/'
+  ? 'https://facer-recognition.herokuapp.com'
+  : 'https://facer-recognition.herokuapp.com'
 
-  //'https://facer-recognition.herokuapp.com/'
+  //'https://facer-recognition.herokuapp.com'
   //'http://localhost:3000'
