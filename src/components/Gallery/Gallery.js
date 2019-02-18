cc683ec89a21723fe71b0c04925f4668ea8663a7
Jonathan Pulido <jonh19h@gmail.com>
2019-02-18 16:48:25 -0600

    ...

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -29,16 +29,16 @@ popo = fetch(`${API_URL}/images`,{
 
 
 render() { 
-const {images} = this.state; 
-return (      
-<div className="gallery m5">
-{images.map((imageObj, i)=> 
-<div key={i}>
-<Image imageObj={imageObj}/>
-</div>
-)} 
-</div>
-);
-}
+	const {images} = this.state; 
+	return (      
+		<div className="gallery m5">
+			{images.map((imageObj, i)=> 
+				<div key={i}>
+					<Image imageObj={imageObj}/>
+				</div>
+			)} 
+		</div>
+		);
+	}
 }
 export default Gallery;
